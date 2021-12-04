2914cb1735ccc3bdde5d1023f2af86f296036b12
Kunal Mahajan <kunalmahajan0901@gmail.com>
2021-12-04 21:53:15 +0530

    changed file structure

--- frontend/src/shared/baseUrl.js
+++
@@ -1,4 +0,0 @@
-// export const baseUrl = "http://localhost:3001/";
-// export const frontendBaseUrl = "http://localhost:3000/"
-export const baseUrl ="https://poogle-app-backend.herokuapp.com/"
-export const frontendBaseUrl = "https://poogle-app.herokuapp.com/"
