837860742d748e9147893b5241739d9278d4712d
Kunal Mahajan <kunalmahajan0901@gmail.com>
2021-12-04 22:06:57 +0530

    updated nessecary files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React from 'react'
-import {BrowserRouter} from 'react-router-dom'
-import Main from './Components/MainComponent'
+import React, { useState } from "react";
+import { BrowserRouter, Router } from "react-router-dom";
+import Main from "./components/main";
+import ScrollToTop from './components/scroll-to-top/scroll-to-top';
+import history from './history';
+import {FaBars} from 'react-icons/fa';
+import Sidebar from './components/sidebar/sidebar'
+import Header from './components/header_footer/header'
+import Footer from './components/header_footer/footer'
+import './App.css';
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
-    </div>
-  )
-}
+function App(props) {
+	const [collapsed, setCollapsed] = useState(false);
+	const [toggled, setToggled] = useState(false);
+	const handleCollapsedChange = (checked) => {
+		setCollapsed(checked);
+	  };
+	const handleToggleSidebar = (value) => {
+		setToggled(value);
+	};
+	
+	
+	return (
+		<BrowserRouter>
+			<Router history={history}>
+			{/* <ScrollToTop> */}
+				<div className={`app  ${toggled? 'toggled':''}`}>
+					<Sidebar collapsed={collapsed} toggled={toggled} handleToggleSidebar={handleToggleSidebar}/>
+					<main>
+						<div className="btn-toggle" onClick={() => handleToggleSidebar(true)}>
+							<FaBars />
+						</div>
+							<Header/>
+							<Main />
+							<Footer/>
+					</main>
+				</div>
+				{/* </ScrollToTop> */}
 
-export default App
-
+			</Router>
+		</BrowserRouter>
+	);
+}	
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.css";
+import "bootstrap-social/bootstrap-social.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider} from "react-redux";
+import { ConfigureStore } from "./redux/configureStore";
+const store = ConfigureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
